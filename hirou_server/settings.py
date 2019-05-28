a113590b5a95a70169c4f47820ee20d47c77fbaa
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2019-05-29 00:02:35 +0530

    Add new collections app

--- hirou_server/settings.py
+++ hirou_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r0+^^(v=m0r!@8hhhg&$uvu(nb279ta^%r+@n8xc6sz#0&*wgm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'collection',
 ]
 
 MIDDLEWARE = [
