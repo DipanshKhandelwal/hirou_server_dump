3558a88d2f9d8f814fd4efbfa61241fd570b8af7
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2019-05-29 00:04:17 +0530

    Update Admin options for collection app's models

--- collection/admin.py
+++ collection/admin.py
@@ -1,1 +1,27 @@
 from django.contrib import admin
+from .models import Vehicle, Item, User, Area, CollectionPoint, Pickup
+
+
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ['registration_number', 'model']
+    ordering = ['registration_number', 'model']
+    fields = ['registration_number', 'model', ('x', 'y')]
+
+
+class PickupAdmin(admin.ModelAdmin):
+    list_display = ['timestamp', 'area', 'vehicle']
+    ordering = ['timestamp', 'area', 'vehicle']
+
+
+class CollectionPointAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address']
+    ordering = ['name', 'address']
+
+
+admin.site.register(
+    [Item, User, Area],
+)
+
+admin.site.register(Vehicle, VehicleAdmin)
+admin.site.register(Pickup, PickupAdmin)
+admin.site.register(CollectionPoint, CollectionPointAdmin)
