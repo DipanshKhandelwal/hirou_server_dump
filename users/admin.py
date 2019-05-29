1467033d6fe7ee16c0584dcb402cb307d08e36f0
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2019-05-29 16:20:37 +0530

    Create CustomRegisterSerializer for user creation

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
+from .models import Profile
 
-def logout_selected_users(modeladmin, request, queryset):
-    queryset.update(is_active=False)
-
-
-admin.site.add_action(logout_selected_users, 'logout_selected_users')
+admin.site.register(Profile)
+admin.site.unregister(Group)
---
+++ users/serializers.py
@@ -0,0 +1,54 @@
+from allauth.account.adapter import get_adapter
+from rest_auth.registration.serializers import RegisterSerializer
+from rest_framework import serializers
+# from rest_framework.authtoken.models import Token
+from django.core.validators import RegexValidator
+
+from .models import User, Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['user', 'gender', 'dob', 'bio']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password', 'phone_number')
+
+
+class CustomRegisterSerializer(RegisterSerializer):
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits "
+                                         "allowed.")
+    phone_number = serializers.CharField(validators=[phone_regex], max_length=17, allow_blank=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password', 'phone_number')
+
+    def get_cleaned_data(self):
+        return {
+            'username': self.validated_data.get('username', ''),
+            'password1': self.validated_data.get('password1', ''),
+            'password2': self.validated_data.get('password2', ''),
+            'email': self.validated_data.get('email', ''),
+            'phone_number': self.validated_data.get('phone_number', '')
+        }
+
+    def save(self, request):
+        adapter = get_adapter()
+        user = adapter.new_user(request)
+        self.cleaned_data = self.get_cleaned_data()
+        user.phone_number = self.cleaned_data.get('phone_number')
+        user.save()
+        adapter.save_user(request, user, self)
+        return user
+
+    def update(self, instance, validated_data):
+        pass
+
+    def create(self, validated_data):
+        pass
