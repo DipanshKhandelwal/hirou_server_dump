f2cdfc9d1256be440792957c378b74034db5ba4c
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2021-04-22 20:04:23 +0530

    Add socket sub event types

--- master/consumers/constants.py
+++ master/consumers/constants.py
@@ -12,3 +12,9 @@ class SocketEventTypes:
     TASK_ROUTE = 'task-route'
     COLLECTION_POINT = 'collection-point'
     TASK_COLLECTION_POINT = 'task-collection-point'
+
+
+class SocketSubEventTypes:
+    REORDER = 'reorder'
+    UPDATE = 'update'
+    CREATE = 'create'
