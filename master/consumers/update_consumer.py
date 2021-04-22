1b5498a803cdd9c0db8dea1f37e52bbc5975a6fa
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2021-04-22 20:12:37 +0530

    Subscribe to both channel on join web socket

--- master/consumers/update_consumer.py
+++ master/consumers/update_consumer.py
@@ -1,9 +1,14 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from .constants import SocketChannels
 
 
 class UpdateConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         await self.accept()
+        await self.send_json({'success': True})
+
+        for channel in SocketChannels.get_all():
+            await self.channel_layer.group_add(channel, self.channel_name)
 
     async def disconnect(self, code):
         await self.close()
