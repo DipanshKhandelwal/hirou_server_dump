31c2c7ca15f8d188fb1361ba7920276064013591
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2021-05-01 17:19:10 +0530

    Add try catch around group send

--- master/consumers/helpers.py
+++ master/consumers/helpers.py
@@ -6,13 +6,16 @@ from .constants import SocketChannels
 def send_update_to_socket(event, sub_event, channel=SocketChannels.COLLECTION_POINT_CHANNEL, data={}):
     channel_layer = get_channel_layer()
 
-    async_to_sync(channel_layer.group_send)(
-        channel, {
-            'type': 'websocket.ingest',
-            'data': {
-                "event": event,
-                "sub-event": sub_event,
-                "data": data,
+    try:
+        async_to_sync(channel_layer.group_send)(
+            channel, {
+                'type': 'websocket.ingest',
+                'data': {
+                    "event": event,
+                    "sub-event": sub_event,
+                    "data": data,
+                }
             }
-        }
-    )
+        )
+    except:
+        print("Error sending socket message")
