19207e559645752a31f9926d73d67bf87e836f7d
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2021-05-03 00:22:56 +0530

    Add TASK_COLLECTION to SocketEventTypes

--- master/consumers/constants.py
+++ master/consumers/constants.py
@@ -12,6 +12,7 @@ class SocketEventTypes:
     TASK_ROUTE = 'task-route'
     COLLECTION_POINT = 'collection-point'
     TASK_COLLECTION_POINT = 'task-collection-point'
+    TASK_COLLECTION = 'task-collection'
 
 
 class SocketSubEventTypes:
